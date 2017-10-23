51c4cb7c5f2733b653a1b5c0f700d6312baea8e4
siddharthachoudhary <siddharthachoudhary@localhost>
2017-10-23 21:48:31 +0530

    icons for menu items

--- src/shelfstockers/MainFrame.java
+++ src/shelfstockers/MainFrame.java
@@ -40,6 +40,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         DesktopPane.setBounds(new java.awt.Rectangle(0, 0, 650, 500));
 
+        BillingMenu.setIcon(new javax.swing.ImageIcon("/Users/siddharthachoudhary/Desktop/Webp.net-resizeimage.png")); // NOI18N
         BillingMenu.setText("Billing");
 
         NewBillingMenu.setText("New Billing");
@@ -52,6 +53,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         jMenuBar1.add(BillingMenu);
 
+        StockMenu.setIcon(new javax.swing.ImageIcon("/Users/siddharthachoudhary/Desktop/data-management-icon.png")); // NOI18N
         StockMenu.setText("Stock");
 
         DisplayMenu.setText("Display Stock");
